823a02acae9aee571984dc358faa52d7cedeca9f
Ilya <iosdsbb@gmail.com>
2026-03-11 00:02:44 +0300

    feat: usage reminder.service in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
 import { TaskListComponent } from "~components/task-list/task-list.component";
 import { ModalHostComponent } from "~components/ui/modal-host/modal-host.component";
+import { ReminderService } from "~data/reminder/services/reminder.service";
 
 @Component({
   selector: "hp-root",
@@ -8,4 +9,10 @@ import { ModalHostComponent } from "~components/ui/modal-host/modal-host.compone
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  readonly #reminderService = inject(ReminderService);
+
+  ngOnInit(): void {
+    this.#reminderService.start();
+  }
+}
